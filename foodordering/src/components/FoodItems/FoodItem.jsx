8d0735c7198c5ee570697ba783bffe7dbeeb5822
sanu <sanu308@gmail.com>
2025-04-04 16:57:28 +0530

    some got to cart changes

--- foodordering/src/components/FoodItems/FoodItem.jsx
+++ foodordering/src/components/FoodItems/FoodItem.jsx
@@ -1,14 +1,20 @@
 import React, { useContext } from 'react'
 import './FoodItem.css'
+import { useNavigate } from 'react-router-dom'
 import { assets } from '../../assets/assets'
 import { StoreContext } from '../../context/StoreContext'
 const FoodItem = ({id,name,price,description,image}) => {
 
-
+  const navigate = useNavigate();
 const{cartItem,addToCart,removeFromCart,url}=useContext(StoreContext)
+
+const goToCart = () => {
+  navigate('/cart');
+}
 
   return (
     <div className="food-item">
+     
         <div className="food-item-img-container">
 <img  className="food-item-image" src={url + "/images/"+image} alt="" />
 
@@ -19,6 +25,14 @@ const{cartItem,addToCart,removeFromCart,url}=useContext(StoreContext)
 <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt=''/>
 <p>{cartItem[id]}</p>
 <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt=''/>
+{Object.keys(cartItem).length > 0 && (
+                <button 
+                    className="go-to-cart-btn" 
+                    onClick={goToCart}
+                >
+                    Go to Cart
+                </button>
+            )}
 </div>
 }
         </div>
@@ -28,7 +42,7 @@ const{cartItem,addToCart,removeFromCart,url}=useContext(StoreContext)
         <img src={assets.rating_starts} alt="" />
     </div>
     <p className="food-item-description">{description}</p>
-    <p className="food-item-price">${price}</p>
+    <p className="food-item-price">₹{price}</p>
 </div>
         </div>  
   )
